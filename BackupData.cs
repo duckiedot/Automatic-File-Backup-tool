5f2c099d761b647ffc68dbf9f615b2e44fdce98a
Duckiedot <m.malinkiewicz01@gmail.com>
2021-12-06 03:57:13 +0100

    Add project files.

---
+++ BackupData.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Zad2
+{
+    public static class BackupData
+    {
+        public static bool BackupSuccesful { get; private set; }
+
+        //keeps track of total backups created and later adds it to directory name like backup2303, backup4003 etc
+        public static int BackupsCreated { get; private set; }
+
+        //local directories 
+        private static string source = @"h:\fakeServer\data";
+        private static string backup = @"h:\fakeServer\receive\backup";
+
+        //copies all files from source dir to an array called filesList, if the copy was succesful it calls SaveFiles()
+        public static void CopyFiles()
+        {
+            DirectoryInfo sourceDirectory = new DirectoryInfo(source);
+            try
+            {
+                FileInfo[] filesList = sourceDirectory.GetFiles();
+                SaveFiles(filesList);
+            }
+            catch (Exception msg)
+            {
+                Console.WriteLine(msg);
+                return;
+            }
+            
+        }
+
+        //saves copied files in directory
+        public static void SaveFiles(FileInfo[] filesList)
+        {
+            //creates directory coresponding to current amount of backups created, references to BackupsCreated prop
+            Directory.CreateDirectory(backup + $"{BackupsCreated}");
+
+            //tries to copy every file into the new dir
+            foreach (FileInfo file in filesList)
+            {
+                try
+                {
+                    string tempPath = Path.Combine((backup + $"{BackupsCreated}"), file.Name);
+                    Console.WriteLine(tempPath);
+                    file.CopyTo(tempPath, false);
+                }
+                catch (Exception)
+                {
+                    BackupSuccesful = false;
+                    return;
+                }
+                
+
+            }
+            //if exception isnt found and the program isnt returned early it adds another Backup to BackupsCreated prop
+            BackupSuccesful = true;
+            BackupsCreated++;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a stopwatch and start it
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            //do a backup only if speficied time elapsed and piervious backup was succesful
+            do
+            {
+                if (!(stopWatch.Elapsed.Seconds > 60))
+                {
+                    Thread.Sleep(6000 - stopWatch.Elapsed.Milliseconds);
+                }
+                BackupData.CopyFiles();
+
+            } while (BackupData.BackupSuccesful);
+        }
+    }
+
+}
